69dd28129ca0fef51da38a9b0c08eaa00c6148c8
anemonadja <nadja.villarroel.n@gmail.com>
2025-07-28 10:28:40 -0400

    link hoja de estilo y cambio de colores

--- datos_personales/index.js
+++ datos_personales/index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*anemonadja.cl");
   next();
 });
 
